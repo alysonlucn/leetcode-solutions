7972b747c2c4b7e5f124f4bf29cd75826e9b10e0
alysonlucn <alysonlucn@gmail.com>
2025-10-16 16:06:45 -0300

    Initial commit with first LeetCode solution

---
+++ src/easy/Solutions.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solutions {
+    //1. Two Sum
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] {i, numMap.get(complement)};
+            }
+            numMap.put(nums[i], i);
+        }
+        return null;
+    }
+}
