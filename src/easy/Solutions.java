62915b8a306b4b68df34f6c1486d8bbca611c951
alysonlucn <alysonlucn@gmail.com>
2025-10-21 13:41:25 -0300

    Solve Remove Element on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -33,4 +33,16 @@ public class Solutions {
         }
         return result;
     }
+
+    //27. Remove Element
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
 }
