36baee1b8c2c0cd1664300a0199be8f513bc6e10
alysonlucn <alysonlucn@gmail.com>
2025-10-30 16:08:35 -0300

    Solve Number of Steps to Reduce a Number to Zero on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -74,4 +74,20 @@ public class Solutions {
         }
         return maximumWealth;
     }
+
+    //1342. Number of Steps to Reduce a Number to Zero
+    public int numberOfSteps(int num) {
+        int count = 0;
+
+        while(num != 0) {
+            if(num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num--;
+            }
+            count++;
+        }
+
+        return count;
+    }
 }
