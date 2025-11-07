56ac9a4d3a14ce5bc4543da9d7014e851274b030
alysonlucn <alysonlucn@gmail.com>
2025-11-07 16:48:02 -0300

    Solve Number of Good Pairs on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -220,4 +220,19 @@ public class Solutions {
 
         return maxWords;
     }
+
+    //1512. Number of Good Pairs
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
