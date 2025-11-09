9cd069d092efa8865f4fc41d478c9ec2a8b7db08
alysonlucn <alysonlucn@gmail.com>
2025-11-09 16:42:05 -0300

    Solve Smaller Numbers Than Current on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -256,4 +256,22 @@ public class Solutions {
 
         return count;
     }
+
+
+    //1365. Smaller Numbers Than Current
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] smallerNumbers = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            smallerNumbers[i] = count;
+        }
+
+        return smallerNumbers;
+    }
 }
