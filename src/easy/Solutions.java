ff6ef64f4681f0eda6639e16ece0f1b3a9740508
alysonlucn <alysonlucn@gmail.com>
2025-11-10 14:31:32 -0300

    Solve Goal Parser Interpretation on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -294,4 +294,19 @@ public class Solutions {
 
         return command;
     }
+
+    //1678. Goal Parser Interpretation
+    public String defangIPaddr(String address) {
+        StringBuilder defanged = new StringBuilder();
+
+        for(int i = 0; i < address.length(); i++) {
+            if(address.charAt(i) == '.') {
+                defanged.append("[.]");
+            } else {
+                defanged.append(address.charAt(i));
+            }
+        }
+
+        return defanged.toString();
+    }
 }
