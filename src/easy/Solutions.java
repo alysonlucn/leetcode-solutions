cb691b98e8cbfe82aefcfe8bc8ef1f7b21d9e7f7
alysonlucn <alysonlucn@gmail.com>
2025-11-10 13:47:56 -0300

    Solve Shuffle the Array on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -274,4 +274,16 @@ public class Solutions {
 
         return smallerNumbers;
     }
+
+    //1470. Shuffle the Array
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+
+        for(int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[i + n];
+        }
+
+        return result;
+    }
 }
