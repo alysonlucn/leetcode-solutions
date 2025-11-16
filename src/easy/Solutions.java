3a963b25e495fb7a7a9d883914a10a362e477502
Alyson <alysonlucn@gmail.com>
2025-11-16 11:43:17 -0300

    Solve Check If N and Its Double Exist on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -455,4 +455,16 @@ public class Solutions {
 
         return totalMatches;
     }
+
+    //1346. Check If N and Its Double Exist
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
