be50583bf403022e5e9b577abc57b512bbf5b4c0
Alyson <alysonlucn@gmail.com>
2025-11-17 14:00:45 -0300

    Solve Shuffle String on LeetCode

--- src/easy/Solutions.java
+++ src/easy/Solutions.java
@@ -467,4 +467,15 @@ public class Solutions {
         }
         return false;
     }
+
+    //1528. Shuffle String
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+
+        for (int i = 0; i < indices.length; i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+
+        return new String(result);
+    }
 }
